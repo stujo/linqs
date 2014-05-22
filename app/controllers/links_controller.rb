50edf76c1ed32bda9453cdde0b2ccaba944a726b
RahulKhanna <sociallyrk@gmail.com>
2014-05-22 16:02:02 -0700

    with each for links view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,44 +1,46 @@
 class LinksController < ApplicationController
-	def main
-	end
+  def main
+  end
 
-	def index
-		@links = Link.all
-	end
+  def index
+    @links = Link.all
+  end
 
-	def new
-		@links = Link.new
-	end
+  def new
+    @links = Link.new
+  end
 
-	def create
-		@link = Link.new(links_params)
-		if @link.save
-			redirect_to link_path
-		else
-			flash[:errors] #something something
-	end
+  def create
+    @link = Link.new(links_params)
+    if @link.save
+      redirect_to link_path
+    else
+      flash[:errors] #something something
+    end
+  end
 
-	def edit
-		@link = Link.find(params[:id])
-	end
+  def edit
+    @link = Link.find(params[:id])
+  end
 
-	def update
-		@link = Link.find(params[:id])
-		if @link.update(link_params)
-			redirect_to link_path(@link)
-		else
-			flash[:errors] = @link.errors.full_messages
-			render :edit
-	end
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      redirect_to link_path(@link)
+    else
+      flash[:errors] = @link.errors.full_messages
+      render :edit
+    end
+  end
 
-	def destroy
-		@link = Link.find(params[:id])
-		@link.destroy
-		redirect_to root_path
-	end
+    def destroy
+      @link = Link.find(params[:id])
+      @link.destroy
+      redirect_to root_path
+    end
 
-	private
-		def links_params
-			params.require(:link).permit(:title, :url)
-		end
-end
+    private
+    def links_params
+      params.require(:link).permit(:title, :url)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,36 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'links#index'
+  resources :links, :tags, :comments
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # get '/signup', to: 'users#new'
+  # delete '/signout', to: 'sessions#destroy'
+  # get'/signin', to: 'sessions#new'
 end
+
+
+#      Prefix Verb  URI Pattern                  Controller#Action
+#        root GET    /                            links#index
+#        links GET    /links(.:format)            links#index
+#              POST  /links(.:format)            links#create
+#    new_link GET    /links/new(.:format)        links#new
+#    edit_link GET    /links/:id/edit(.:format)    links#edit
+#        link GET    /links/:id(.:format)        links#show
+#              PATCH  /links/:id(.:format)        links#update
+#              PUT    /links/:id(.:format)        links#update
+#              DELETE /links/:id(.:format)        links#destroy
+#        tags GET    /tags(.:format)              tags#index
+#              POST  /tags(.:format)              tags#create
+#      new_tag GET    /tags/new(.:format)          tags#new
+#    edit_tag GET    /tags/:id/edit(.:format)    tags#edit
+#          tag GET    /tags/:id(.:format)          tags#show
+#              PATCH  /tags/:id(.:format)          tags#update
+#              PUT    /tags/:id(.:format)          tags#update
+#              DELETE /tags/:id(.:format)          tags#destroy
+#    comments GET    /comments(.:format)          comments#index
+#              POST  /comments(.:format)          comments#create
+#  new_comment GET    /comments/new(.:format)      comments#new
+# edit_comment GET    /comments/:id/edit(.:format) comments#edit
+#      comment GET    /comments/:id(.:format)      comments#show
+#              PATCH  /comments/:id(.:format)      comments#update
+#              PUT    /comments/:id(.:format)      comments#update
+#              DELETE /comments/:id(.:format)      comments#destroy
