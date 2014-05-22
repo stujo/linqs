2c7648ce6163be63fa78d6f91b954107be40379d
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-22 16:06:03 -0700

    added tag routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,26 @@ Rails.application.routes.draw do
   # get'/signin', to: 'sessions#new'
 end
 
-
-#      Prefix Verb  URI Pattern                  Controller#Action
-#        root GET    /                            links#index
-#        links GET    /links(.:format)            links#index
-#              POST  /links(.:format)            links#create
-#    new_link GET    /links/new(.:format)        links#new
+#       Prefix Verb   URI Pattern                  Controller#Action
+#         root GET    /                            links#index
+#        links GET    /links(.:format)             links#index
+#              POST   /links(.:format)             links#create
+#     new_link GET    /links/new(.:format)         links#new
 #    edit_link GET    /links/:id/edit(.:format)    links#edit
-#        link GET    /links/:id(.:format)        links#show
-#              PATCH  /links/:id(.:format)        links#update
-#              PUT    /links/:id(.:format)        links#update
-#              DELETE /links/:id(.:format)        links#destroy
-#        tags GET    /tags(.:format)              tags#index
-#              POST  /tags(.:format)              tags#create
+#         link GET    /links/:id(.:format)         links#show
+#              PATCH  /links/:id(.:format)         links#update
+#              PUT    /links/:id(.:format)         links#update
+#              DELETE /links/:id(.:format)         links#destroy
+#         tags GET    /tags(.:format)              tags#index
+#              POST   /tags(.:format)              tags#create
 #      new_tag GET    /tags/new(.:format)          tags#new
-#    edit_tag GET    /tags/:id/edit(.:format)    tags#edit
+#     edit_tag GET    /tags/:id/edit(.:format)     tags#edit
 #          tag GET    /tags/:id(.:format)          tags#show
 #              PATCH  /tags/:id(.:format)          tags#update
 #              PUT    /tags/:id(.:format)          tags#update
 #              DELETE /tags/:id(.:format)          tags#destroy
-#    comments GET    /comments(.:format)          comments#index
-#              POST  /comments(.:format)          comments#create
+#     comments GET    /comments(.:format)          comments#index
+#              POST   /comments(.:format)          comments#create
 #  new_comment GET    /comments/new(.:format)      comments#new
 # edit_comment GET    /comments/:id/edit(.:format) comments#edit
 #      comment GET    /comments/:id(.:format)      comments#show
