7e04e7402ca200413a8f5661040e6ee0ec29c9f0
RahulKhanna <sociallyrk@gmail.com>
2014-05-23 10:21:08 -0700

    tags controller added

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
   has_many :link_tags
   has_many :tags, through: :link_tags
   has_many :comments, as: :commentable
+  accepts_nested_attributes_for :link_tags
 end
--- app/models/link_tag.rb
+++ app/models/link_tag.rb
@@ -1,4 +1,5 @@
 class LinkTag < ActiveRecord::Base
   belongs_to :link
   belongs_to :tag
+  accepts_nested_attributes_for :tag
 end
