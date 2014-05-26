0cd879bde244d84fa478d316f2b1c8f80dce87c7
shaker <shakalghaithi@gmail.com>
2014-05-26 12:31:43 -0700

    bootstrap

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,20 +1,31 @@
 class LinksController < ApplicationController
+  before_action :find_link , only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:index, :show]
   def main
   end
 
   def index
-    @links = Link.all
+
+     # @links = Link.includes((:tag).where.not{:name => "private"})
+       # @links = Link.all
+       # Link.where(:link_id => Link.id, tag.name => " private")
+         @links.each do |link|
+         @tag = link.tags.where.not(name: "private")
+       # :domains => { :name => "pauls-domain"}})
+       
+      end
   end
 
   def new
     @link = Link.new
+    #@link.user = current_user
     @link_tag = @link.link_tags.build
     @link_tag.build_tag
   end
 
   def create
     @link = Link.new(links_params)
-    if @link.save
+    if @link.save 
       redirect_to link_path(@link)
     else
       flash[:errors] = @link.errors.full_messages
@@ -22,19 +33,19 @@ class LinksController < ApplicationController
   end
 
   def show
-    @link = Link.find(params[:id])
+    #@link = Link.find(params[:id])
     @title = @link.title
     @url = @link.url
   end
 
   def edit
-    @link = Link.find(params[:id])
+   # @link = Link.find(params[:id])
     @link_tag = @link.link_tags.build
     @link_tag.build_tag
   end
 
   def update
-    @link = Link.find(params[:id])
+    #@link = Link.find(params[:id])
     if @link.update(links_params)
       redirect_to link_path(@link)
     else
@@ -44,13 +55,35 @@ class LinksController < ApplicationController
   end
 
     def destroy
-      @link = Link.find(params[:id])
+      #@link = Link.find(params[:id])
       @link.destroy
       redirect_to root_path
     end
+ #  def destroy
+   #   @link = Link.find(params[:id])
+     
+   #   if @link.user != current_user
+   #     flash[:notice] = 'That link is not yours to destory'
+
+   #     respond_to do |format|
+   #       format.html { redirect_to :singin_path }
+   #       format.json { head :no_content }
+   #     end
+   #   else
+   #     @link.destroy
+
+   #     respond_to do |format|
+   #       format.html { redirect_to links_url }
+   #       format.json { head :no_content }
+   #     end
+   #   end
+ #   end
 
     private
+    def find_link
+      @link = Link.find(params[:id])
+    end
     def links_params
-      params.require(:link).permit(:title, :url, link_tags_attributes: [tags_attributes: [:name] ])
+      params.require(:link).permit(:title, :url, link_tags_attributes: [tag_attributes: [:name] ])
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140523000740) do
 
   create_table "tags", force: true do |t|
     t.string   "name"
+    t.string   "tag_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
