5a9cffc571d144ed3f34a96a2f99a06629209e57
RahulKhanna <sociallyrk@gmail.com>
2014-05-26 12:45:20 -0700

    with rspecs

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,20 +1,24 @@
 class LinksController < ApplicationController
   before_filter :authenticate_user!
   #before_filter :check_link_owner, only: [:edit, :update, :destroy]
+  # not currently being used; we may want to set our SPA homepage to main
 
   def main
   end
 
+# will eventually display all public links via ajax get request
   def index
     @links = Link.all
   end
 
+# should build new tags associated with the new link - is this working??
   def new
     @link = Link.new
     @link_tag = @link.link_tags.build
     @link_tag.build_tag
   end
 
+# will send new instance of link via ajax post to db and retrieves json for same link to display on page without reloading
   def create
     @link = Link.new(links_params)
     @link[:user_id] = current_user.id
@@ -25,16 +29,19 @@ class LinksController < ApplicationController
     end
   end
 
+# shows details of a single link, including all tags assoc'd with it
   def show
     @link = Link.find(params[:id])
     @title = @link.title
     @url = @link.url
   end
 
+# loads link and assoc'd tags to edit
   def edit
     @link = Link.find(params[:id])
   end
 
+ # will send updated link attributes via ajax post 
   def update
     @link = Link.find(params[:id])
     if @link.update(links_params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-  # validates :name, :login, :email, :presence => true
-  # validates :email, :confirmation => true
-  # validates :email_confirmation, :presence => true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
