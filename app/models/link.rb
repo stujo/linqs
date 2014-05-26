9ee130ae84f6e1e1687901faa6846ffc13c0bd8f
RahulKhanna <sociallyrk@gmail.com>
2014-05-26 13:58:34 -0700

    with user model changes

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ActiveRecord::Base
   has_many :link_tags
   has_many :tags, through: :link_tags
   has_many :comments, as: :commentable
+  belongs_to :user
   accepts_nested_attributes_for :link_tags
 
   validates :title, presence: true, length: {minimum: 2}
